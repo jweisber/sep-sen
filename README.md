dc155a4edee9b295fd6f2d98e3f5ed5fb985ad9c
Jonathan Weisberg <jonathan.weisberg@utoronto.ca>
2020-11-02 11:33:29 -0500

    minor tweaks

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Code to simulate some epistemic networks and reproduce results from [Zollman (2007)](https://www.doi.org/10.1086/525605), [Rosenstock, Bruner, and O'Connor (2017)](http://doi.org/10.1086/690717), and [O'Connor & Weatherall (2018)](https://doi.org/10.1007/s13194-018-0213-9). See [these blog posts](https://jonathanweisberg.org/post/zollman/) for discussion and explanation of the results.
+Code to simulate some epistemic networks and reproduce results from:
+
+- [Zollman (2007)](https://www.doi.org/10.1086/525605),
+- [Rosenstock, Bruner, and O'Connor (2017)](http://doi.org/10.1086/690717), and
+- [O'Connor & Weatherall (2018)](https://doi.org/10.1007/s13194-018-0213-9).
+
+See [these blog posts](https://jonathanweisberg.org/post/zollman/) for discussion and explanation of the results.
--- results/rbo.R
+++ results/rbo.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(hrbrthemes)
-theme_set(theme_ipsum())
+theme_set(theme_ipsum(base_size = 14))
 
 # Fig 2 ----
 df2 <- read_csv("rbo-fig-2.csv") %>%
@@ -11,7 +11,10 @@ df2 <- read_csv("rbo-fig-2.csv") %>%
 
 ggplot(df2) +
   geom_path(aes(x = epsilon, y = p, colour = graph)) +
-  scale_color_brewer(palette = "Set1")
+  scale_color_brewer(palette = "Set1") +
+  xlab("Epsilon") + ylab("Probability of True Consensus") +
+  labs(colour = "Shape") +
+  theme(axis.title.x = element_text(size = 12), axis.title.y = element_text(size = 12))
 
 ggsave("rbo-2.png", width = 10, height = 7)
 
@@ -24,7 +27,10 @@ df3 <- read_csv("rbo-fig-3.csv") %>%
 
 ggplot(df3) +
   geom_path(aes(x = trials, y = p, colour = graph)) +
-  scale_color_brewer(palette = "Set1")
+  scale_color_brewer(palette = "Set1") +
+  xlab("Trials") + ylab("Probability of True Consensus") +
+  labs(colour = "Shape") +
+  theme(axis.title.x = element_text(size = 12), axis.title.y = element_text(size = 12))
 
 ggsave("rbo-3.png", width = 10, height = 7)
 
@@ -37,6 +43,9 @@ df4 <- read_csv("rbo-fig-4.csv") %>%
 
 ggplot(df4) +
   geom_path(aes(x = agents, y = p, colour = graph)) +
-  scale_color_brewer(palette = "Set1")
+  scale_color_brewer(palette = "Set1") +
+  xlab("Number of Agents") + ylab("Probability of True Consensus") +
+  labs(colour = "Shape") +
+  theme(axis.title.x = element_text(size = 12), axis.title.y = element_text(size = 12))
 
 ggsave("rbo-4.png", width = 10, height = 7)
